4abc26e5258a8998ea192ad4d44399826ef45279
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2022-02-02 18:19:46 +0300

    Iterator

---
+++ Behavior/Iterator.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behavior
+{
+    namespace Iterator
+    {
+    }
+}
+
+/*
+Паттерн Итератор (Iterator) предоставляет абстрактный интерфейс для последовательного доступа 
+ко всем элементам составного объекта без раскрытия его внутренней структуры.
+
+    class Client
+    {
+        public void Main()
+        {
+            Aggregate a = new ConcreteAggregate();
+             
+            Iterator i = a.CreateIterator();
+ 
+            object item = i.First();
+            while (!i.IsDone())
+            {
+                item = i.Next();
+            }
+        }
+    }
+ 
+    abstract class Aggregate
+    {
+        public abstract Iterator CreateIterator();
+        public abstract int Count { get; protected set; }
+        public abstract object this[int index] { get; set; }
+    }
+  
+    class ConcreteAggregate : Aggregate
+    {
+        private readonly ArrayList _items = new ArrayList();
+  
+        public override Iterator CreateIterator()
+        {
+            return new ConcreteIterator(this);
+        }
+  
+        public override int Count
+        {
+            get { return _items.Count; }
+            protected set { }
+        }
+ 
+        public override object this[int index]
+        {
+            get { return _items[index]; }
+            set { _items.Insert(index, value); }
+        }
+    }
+
+    abstract class Iterator
+    {
+        public abstract object First();
+        public abstract object Next();
+        public abstract bool IsDone();
+        public abstract object CurrentItem();
+    }
+  
+    class ConcreteIterator : Iterator
+    {
+        private readonly Aggregate _aggregate;
+        private int _current;
+  
+        public ConcreteIterator(Aggregate aggregate)
+        {
+            this._aggregate = aggregate;
+        }
+  
+        public override object First()
+        {
+            return _aggregate[0];
+        }
+  
+        public override object Next()
+        {
+            object ret = null;
+  
+            _current++;
+  
+            if (_current < _aggregate.Count)
+            {
+                ret = _aggregate[_current];
+            }
+  
+            return ret;
+        }
+  
+        public override object CurrentItem()
+        {
+            return _aggregate[_current];
+        }
+  
+        public override bool IsDone()
+        {
+            return _current >= _aggregate.Count;
+        }
+    }
+
+Iterator: определяет интерфейс для обхода составных объектов
+
+Aggregate: определяет интерфейс для создания объекта-итератора
+
+ConcreteIterator: конкретная реализация итератора для обхода объекта Aggregate.
+Для фиксации индекса текущего перебираемого элемента использует целочисленную переменную _current
+
+ConcreteAggregate: конкретная реализация Aggregate. Хранит элементы, которые надо будет перебирать
+
+Client: использует объект Aggregate и итератор для его обхода
+*/
