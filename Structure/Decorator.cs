0fcc013b956b3576b080242a115f5a4d4bcca708
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2022-02-02 18:00:08 +0300

    Decorator

---
+++ Structure/Decorator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure
+{
+    namespace Decorator
+    {
+    }
+}
+
+/*
+Декоратор (Decorator) представляет структурный шаблон проектирования, который позволяет динамически подключать к объекту дополнительную функциональность.
+
+Для определения нового функционала в классах нередко используется наследование. Декораторы же предоставляет наследованию более гибкую альтернативу, поскольку позволяют динамически в процессе выполнения определять новые возможности у объектов.
+
+
+    abstract class Component
+    {
+        public abstract void Operation();
+    }
+
+    class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {}
+    }
+
+    abstract class Decorator : Component
+    {
+        protected Component component;
+ 
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+ 
+        public override void Operation()
+        {
+            if (component != null)
+                component.Operation();
+        }
+    }
+
+    class ConcreteDecoratorA : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+        }
+    }
+
+    class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+        }
+    }
+
+Component: абстрактный класс, который определяет интерфейс для наследуемых объектов
+
+ConcreteComponent: конкретная реализация компонента, в которую с помощью декоратора добавляется новая функциональность
+
+Decorator: собственно декоратор, реализуется в виде абстрактного класса и имеет тот же базовый класс, что и декорируемые объекты. Поэтому базовый класс Component должен быть по возможности легким и определять только базовый интерфейс.
+
+Класс декоратора также хранит ссылку на декорируемый объект в виде объекта базового класса Component и реализует связь с базовым классом как через наследование, так и через отношение агрегации.
+
+Классы ConcreteDecoratorA и ConcreteDecoratorB представляют дополнительные функциональности, которыми должен быть расширен объект ConcreteComponent.
+*/
