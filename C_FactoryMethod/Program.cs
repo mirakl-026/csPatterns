ad795eaa7d1a25952b0dfec885d44ebf4211fb50
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2022-02-02 16:47:49 +0300

    Singleton

--- C_FactoryMethod/Program.cs
+++ C_FactoryMethod/Program.cs
@@ -3,23 +3,40 @@ using System.Collections.Generic;
 
 using Creation.FactoryMethod;
 using Creation.AbstractFactory;
+using Creation.Singleton;
 
 class Program
 {
     static void Main(string[] args)
     {
+        #region FactoryMethod
+        // FactoryMethod
         //Builder tom = new WoodBuilder("Tom");
         //tom.Create();
+        #endregion
 
-        Hero elf = new Hero(new ElfFactory());
-        elf.Hit();
-        elf.Run();
+        #region AbstractFactory
+        //Hero elf = new Hero(new ElfFactory());
+        //elf.Hit();
+        //elf.Run();
 
-        Hero orc = new Hero(new WarriorFactory());
-        orc.Hit();
-        orc.Run();
+        //Hero orc = new Hero(new WarriorFactory());
+        //orc.Hit();
+        //orc.Run();
 
-        Console.ReadLine();
+        //Console.ReadLine();
+        #endregion
+
+        #region Singleton
+
+        Gateway gateway = Gateway.getInstance("North gates");
+        Console.WriteLine(gateway.Name);
+
+        Gateway gateway1 = Gateway.getInstance("South gates");
+        Console.WriteLine(gateway1.Name);   // same
+        #endregion
     }
 }
+
+
 
---
+++ C_FactoryMethod/Singleton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creation
+{
+    namespace Singleton
+    {
+        class Gateway
+        {
+            private static Gateway instance;
+            public string Name { get; private set; }
+
+            private Gateway(string name)
+            { 
+                this.Name = name;
+            }
+
+            public static Gateway getInstance(string name)
+            {
+                if (instance == null)
+                    instance = new Gateway(name);
+                return instance;
+            }
+        }
+    }
+}
+
+/*
+Одиночка (Singleton, Синглтон) - порождающий паттерн, который гарантирует, что для определенного класса будет создан только один объект,
+а также предоставит к этому объекту точку доступа.
+
+Синглтон позволяет создать объект только при его необходимости. Если объект не нужен, то он не будет создан. 
+В этом отличие синглтона от глобальных переменных.
+
+
+    class Singleton
+    {
+        private static Singleton instance;
+ 
+        private Singleton()
+        {}
+ 
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+    }
+
+*/
